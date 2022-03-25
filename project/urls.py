05ceb72a9b5639609027ad0a393b1457e3525b72
Dominic Fanucchi <dominic_fanucchi@yahoo.com>
2022-03-24 18:42:16 -0700

    testing pushes

--- project/urls.py
+++ project/urls.py
@@ -5,8 +5,6 @@ from . import views
 app_name = 'senior project'
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
-
-    path('speech/', views.speech, name='speech'),
-    
+    path('speech', views.speech, name='speech'),
     path('nlp', views.nlp, name='nlp'),
 ]

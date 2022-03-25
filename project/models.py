3a64407636cfdec1086689b1a8a4700a9c839b3a
Dominic Fanucchi <dominic_fanucchi@yahoo.com>
2022-03-25 04:14:58 -0700

    added database model to store speech for analysis

--- project/models.py
+++ project/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# Represents the file structure in our database
+
+class Speech_Analysis(models.Model):
+    date_created =  models.DateTimeField(auto_now_add=True, null=True)
+    speech_to_be_analyzed = models.CharField(max_length=20000, null=True)

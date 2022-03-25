2be1a70b376e8aa0faf9fc7a36aa127fb03bbefe
Dominic Fanucchi <dominic_fanucchi@yahoo.com>
2022-03-25 07:05:30 -0700

    database models

--- project/models.py
+++ project/models.py
@@ -4,5 +4,20 @@ from django.db import models
 # Represents the file structure in our database
 
 class Speech_Analysis(models.Model):
+    name = models.CharField(max_length=100, null=True)
     date_created =  models.DateTimeField(auto_now_add=True, null=True)
     speech_to_be_analyzed = models.CharField(max_length=20000, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class nlp(models.Model):
+    STATUS = (
+        ('PENDING', 'Pending'),
+        ('ANALYZED', 'Analyzed')
+    )
+    name = models.CharField(max_length=100, null=True)
+    date_created = models.CharField(max_length=20000, null=True)
+    # category = models.CharField(max_length=100, null=True, )
+    status = models.CharField(max_length=100, null=True, choices=STATUS)
--- senior_project/settings.py
+++ senior_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#(!p13_2bt=#@vy^@p9m1(*_@p7hd_=#@cc@zlc_*3i#ts&4^a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
